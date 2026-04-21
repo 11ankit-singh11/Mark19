edd44b03cbe675accb4f472cfc19a14936db02b8
Ankit Singh <ankitsinghsr19@gmail.com>
2026-04-21 15:44:25 +0530

    updated configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import express from "express";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app = express();
+const app = express()
 
-// middlewares
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({limit: "16kb", extended: true}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
 
 
-export default app;
+
